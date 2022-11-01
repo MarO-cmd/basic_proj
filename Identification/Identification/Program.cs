1a6fa65868954ac2b1719f421fba3cdb98fa0a8c
Marclin0 <106659477+Marclin0@users.noreply.github.com>
2022-11-01 22:30:54 +0200

    Add files via upload

---
+++ Identification/Identification/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Identification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Enter Your Id : ");
+                    string id = Console.ReadLine();
+                    Id_analysis id1 = new Id_analysis(id);
+                    id1.Display();
+                }
+            }
+            catch (Wrong_Id ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
