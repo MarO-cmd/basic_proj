a88a985455116db2fc3974af7367cfeca90d4467
Mar0 <106659477+MarO-cmd@users.noreply.github.com>
2022-11-06 17:31:12 +0200

    Update Program.cs

--- Identification/Identification/Program.cs
+++ Identification/Identification/Program.cs
@@ -6,27 +6,28 @@ namespace Identification
     {
         static void Main(string[] args)
         {
-            try
-            {
-                while (true)
+            while(true)
+            {    
+                try
                 {
-                    Console.Write("Enter Your Id : ");
-                    string id = Console.ReadLine();
-                    Id_analysis id1 = new Id_analysis(id);
-                    id1.Display();
+                        
+                 Console.Write("Enter Your Id : ");
+                 string id = Console.ReadLine();
+                 Id_analysis id1 = new Id_analysis(id);
+                 id1.Display();
+                
+                }
+                catch (Wrong_Id ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
-            catch (Wrong_Id ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
 
-            Console.ReadKey();
-
+           
         }
     }
 }
